7989c681d935f01a93fa3812b633bb00a4cbe2d8
Victor Mateus <victormateus897@gmail.com>
2024-09-04 01:05:56 -0300

    Feat: Swagger Dark Mode

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as fs from 'fs';
+import * as path from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,10 +12,19 @@ async function bootstrap() {
     .setTitle('Create Posts')
     .setDescription('Create posts simple api')
     .setVersion('1.0')
-    /*     .addTag('Others')
-     */ .build();
+    .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+
+  // Use o caminho absoluto para o arquivo CSS
+  const customCss = fs.readFileSync(
+    path.join(__dirname, '..', 'src', 'assets', 'SwaggerDark.css'),
+    'utf8',
+  );
+
+  // Setup do Swagger com CSS customizado
+  SwaggerModule.setup('api', app, document, {
+    customCss,
+  });
 
   await app.listen(3000);
 }
